f7ac698590adf1a54d3095c3036eb5b180bc78e6
Garret Benoit <gbenoit@gmail.com>
2020-11-29 16:15:07 -0600

    ghjk

--- api.py
+++ api.py
@@ -51,15 +51,7 @@ def power():
             response=str(s.recv(24))
             print("Response from the Speakers():", response)
             s.close
-						s.connect((HOST2, PORT2))
-						s.sendall(f"*Z0{x}ON\r".encode())
-						s.close
-						s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-						s.connect((HOST2, PORT2))
-						s.sendall(f"*Z0{x}VOL20\r".encode())
-						response=str(s.recv(24))
-						print("Response from the Speakers():", response)
-						s.close
+            time.sleep(1)
         if zpwr is False:
             s=socket.socket( socket.AF_INET, socket.SOCK_STREAM)
             s.connect((HOST2, PORT2))
