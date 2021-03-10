3fbecbcdb9db837ed1c6d6852fcb5bb28a2a2287
garebearvol <gbenoit@gmail.com>
2021-03-10 10:42:58 -0600

    a

--- api.py
+++ api.py
@@ -30,10 +30,9 @@ def off():
 @app.route("/control", methods=["POST"])
 def power():
     data= request.get_json()
-    def power_change(**kwargs):
-        r=kwargs.get('post_data', {})
+    def power_change():
         for x in range (1, 7):
-            zpwr=kwargs.get('pwr{}'.format(x))
+            zpwr=data.get('pwr{}'.format(x))
             if zpwr is True:
                 s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((HOST2, PORT2))
@@ -51,7 +50,7 @@ def power():
                 s.sendall("*Z0{}OFF\r".format(x).encode())
                 s.close
                 time.sleep(1)
-    thread=Thread(target=power_change, kwargs=request.get_json())
+    thread=Thread(target=power_change)
     thread.start()
     return "Started"
             
