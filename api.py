eb0fb64ec5503bd2b7728dabe9661623dfc1541f
garebearvol <gbenoit@gmail.com>
2021-03-10 08:27:43 -0600

    Flask threading

--- api.py
+++ api.py
@@ -2,9 +2,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask import jsonify, request
-import socket
-import json
-import time
+import socket, json, time, threading
 
 HOST2='192.168.1.115'
 PORT2=4999
@@ -33,28 +31,31 @@ def off():
 def power():
     r= request.get_json()
     response=[]
-    for x in range (1, 7):
-        response=[]
-        zpwr=r.get('pwr{}'.format(x))
-        if zpwr is True:
-            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}ON\r".format(x).encode())
-            s.close
-            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}VOL20\r".format(x).encode())
-            response=str(s.recv(24))
-            print("Response from the Speakers():", response)
-            s.close
-            time.sleep(1)
-        if zpwr is False:
-            s=socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}OFF\r".format(x).encode())
-            s.close
-            time.sleep(1)
-    return str(response)
+    def power_change():
+        for x in range (1, 7):
+            response=[]
+            zpwr=r.get('pwr{}'.format(x))
+            if zpwr is True:
+                s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST2, PORT2))
+                s.sendall("*Z0{}ON\r".format(x).encode())
+                s.close
+                s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST2, PORT2))
+                s.sendall("*Z0{}VOL20\r".format(x).encode())
+                response=str(s.recv(24))
+                print("Response from the Speakers():", response)
+                s.close
+                time.sleep(1)
+            if zpwr is False:
+                s=socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST2, PORT2))
+                s.sendall("*Z0{}OFF\r".format(x).encode())
+                s.close
+                time.sleep(1)
+    thread=threading.Thread(target=power_change)
+    thread.start()
+    return {"message": "Accepted"}, 202
             
 
 api.add_resource(status, '/status/')
