fadb24bca7b64f391becc97fdf98a1d193456f34
Garret Benoit <gbenoit@gmail.com>
2021-03-13 08:38:52 -0600

    vdvdsvs

--- api.py
+++ api.py
@@ -10,6 +10,27 @@ PORT2=4999
 app = Flask(__name__)
 api = Api(app)
 
+def power_change(data):
+    for x in range (1, 7):
+        zpwr=data.get('pwr{}'.format(x))
+        if zpwr is True:
+            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST2, PORT2))
+            s.sendall("*Z0{}ON\r".format(x).encode())
+            s.close
+            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST2, PORT2))
+            s.sendall("*Z0{}VOL20\r".format(x).encode())
+            response=str(s.recv(24))
+            s.close
+            time.sleep(1)
+        if zpwr is False:
+            s=socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST2, PORT2))
+            s.sendall("*Z0{}OFF\r".format(x).encode())
+            s.close
+            time.sleep(1)
+
 class status(Resource):
     def status():
         if request.method=='POST':
@@ -32,26 +53,7 @@ def power():
     data= request.get_json()
     Thread(target=power_change, args=(data,)).start()
     return jsonify('Response asynchronosly')
-def power_change(args):
-    for x in range (1, 7):
-        zpwr=args.get('pwr{}'.format(x))
-        if zpwr is True:
-            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}ON\r".format(x).encode())
-            s.close
-            s =socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}VOL20\r".format(x).encode())
-            response=str(s.recv(24))
-            s.close
-            time.sleep(1)
-        if zpwr is False:
-            s=socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST2, PORT2))
-            s.sendall("*Z0{}OFF\r".format(x).encode())
-            s.close
-            time.sleep(1)
+
             
 
 api.add_resource(status, '/status/')
