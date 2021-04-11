ab67268b42fd658238abf9619fa498eec784716c
Garret Benoit <gbenoit@gmail.com>
2021-04-11 14:03:36 -0500

    moved open close to by command

--- api.py
+++ api.py
@@ -19,15 +19,19 @@ ser = serial.Serial(
 
 def power_change(data):  
     for x in range (1, 7):
-        ser.close()
-        ser.open()
         zpwr=data.get('pwr{}'.format(x))
         if zpwr is True:
+            ser.close()
+            ser.open()
             ser.write("*Z0{}ON\r".format(x))
             #time.sleep(1)
+            ser.close()
+            ser.open()
             ser.write("*Z0{}VOL20\r".format(x))
             #time.sleep(1)
         if zpwr is False:
+            ser.close()
+            ser.open()
             ser.write("*Z0{}OFF\r".format(x))
             #time.sleep(1)
             
